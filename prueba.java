f35a3392245c6b90f5266b59672e501e9462ed9f
Fernando Mora <fmora.utch@gmail.com>
2022-03-23 20:55:04 -0700

    agregamos una linea

--- prueba.java
+++ prueba.java
@@ -2,5 +2,6 @@
 class prueba{
     public static void main(String[] args){
        System.out.println("Esta es la practica dos de jenkins, saludos");
+       System.out.println("La conexion con Github se ha hecho");
     }
 }
